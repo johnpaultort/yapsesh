de6f866a9af67e35e26a47cf1485f108cfc22f0e
johnpaultort <johnpaultort@csu.fullerton.edu>
2026-07-08 15:50:32 -0700

    I have added something a little bit more challenging for this project. So far I have thought outside of the box on what is going to seperate my blog from others. As of right now I am shooting for something that looks static, then writing down on my personal notebook ideas that I am hoping to reach. I have added something like twitter where I can write down what I want to express. This is going to take some time, but it will get done.

---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+const galleries = {
+
+    programming: [
+
+        {
+            image: "images/programming/snowfitter.png",
+            title: "Snowfitter",
+            description: "My ski recommendation website."
+        },
+
+        {
+            image: "images/programming/lexer.png",
+            title: "Lexer",
+            description: "Compiler class project."
+        }
+
+    ],
+
+    cycling: [
+
+        {
+            image: "images/cycling/ride1.jpg",
+            title: "Palos Verdes",
+            description: "35-mile Saturday ride."
+        },
+
+        {
+            image: "images/cycling/ride2.jpg",
+            title: "BMC TeamMachine",
+            description: "My current road bike."
+        }
+
+    ],
+
+    outdoors: [
+
+        {
+            image: "images/outdoors/ski1.jpg",
+            title: "Copper Mountain",
+            description: "Favorite ski trip."
+        },
+
+        {
+            image: "images/outdoors/hike.jpg",
+            title: "Hiking",
+            description: "Weekend adventure."
+        }
+
+    ]
+
+};
